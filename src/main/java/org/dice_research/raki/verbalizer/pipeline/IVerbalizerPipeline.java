9413d361e57f021b510ab72a2e4cc3a9c3d78ec1
Rene Speck <speckrene@gmail.com>
2021-04-27 10:26:27 +0200

    Update.

--- src/main/java/org/dice_research/raki/verbalizer/pipeline/IVerbalizerPipeline.java
+++ src/main/java/org/dice_research/raki/verbalizer/pipeline/IVerbalizerPipeline.java
@@ -1,17 +1,18 @@
 package org.dice_research.raki.verbalizer.pipeline;
 
-import org.aksw.owl2nl.data.IInput;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.dice_research.raki.verbalizer.pipeline.data.input.IRAKIInput;
 import org.dice_research.raki.verbalizer.pipeline.data.output.IOutput;
 
 public interface IVerbalizerPipeline {
 
   Logger LOG = LogManager.getLogger(IVerbalizerPipeline.class);
 
-  IVerbalizerPipeline setInput(final IInput input);
+  IVerbalizerPipeline setInput(final IRAKIInput input);
 
   IVerbalizerPipeline setOutput(final IOutput<?> output);
 
   IVerbalizerPipeline run();
+
 }
